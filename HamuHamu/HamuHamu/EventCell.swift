6f41d4c291805f052193f6ea9079dd8e54185736
ywangnon <ywangnon123@gmail.com>
2019-12-26 22:11:59 +0900

    create EventCell

---
+++ HamuHamu/HamuHamu/EventCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventCell.swift
+//  HamuHamu
+//
+//  Created by Hansub Yoo on 2019/12/26.
+//  Copyright © 2019 hansub yoo. All rights reserved.
+//
+
+import UIKit
+
+class EventCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
